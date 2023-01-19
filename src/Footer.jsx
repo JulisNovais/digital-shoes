ddb0c954596ad7bfa089f305062f1a85fb8ab3d8
Giovanne Mendonça <giovannemendonca@outlook.com>
2023-01-19 19:46:21 -0300

    giovanne

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,14 +1,14 @@
 function Footer() {
- 
-    return (
-      <div>
-        FOOTER <br></br>
-        <img src={ require('/Users/pedrogoncalves/GitHub/digital-shoes/src/chinelo.jpg') } /> <br />
-        Chinela do Lampião - Esgotada !!
-        allan
-        Lord of the Rings
-      </div>
-    );
-  }
-  
-  export default Footer;
+  return (
+    <div>
+      FOOTER <br></br>
+      <img
+        src={require("/Users/pedrogoncalves/GitHub/digital-shoes/src/chinelo.jpg")}
+      />{" "}
+      <br />
+      Chinela do Lampião - Esgotada !! allan giovanne
+    </div>
+  );
+}
+
+export default Footer;
