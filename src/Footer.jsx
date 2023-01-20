78923cf2946fc894da2c2b5f63f3ca0b9e2e1057
angelolustosa <angelolustosa@gmail.com>
2023-01-19 21:02:21 -0300

    abcd

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -3,7 +3,6 @@ function Footer() {
     return (
       <div>
         FOOTER <br></br>
-        <img src={ require('/Users/pedrogoncalves/GitHub/digital-shoes/src/chinelo.jpg') } /> <br />
         Chinela do Lampião - Esgotada !!
         allan
         Lord of the Rings
