348189163284f508c0ea3f197731723e58980fd5
CMCCVR <cmccvr@gmail.com>
2023-01-24 20:19:13 -0300

    inclusão de nome - teste

--- src/Products.jsx
+++ src/Products.jsx
@@ -1,11 +1,12 @@
 function Products() {
   return (     
     <div>
-        <h2>Ténis</h2>
+        <h2>Produtos Relacionados</h2>
           <section>
             <div>
               <img src="https://imgcentauro-a.akamaihd.net/400x400/9789492R/tenis-nike-revolution-6-masculino-img.jpg" alt=""/>
-              <p></p>
+              <p>Ténis</p>
+              <p> K-Swiss V8 - Masculino</p>
               <p>Price : <span>R$50</span></p>
               {/* <link to>"/Carrinho</link>   */}
             </div> 
