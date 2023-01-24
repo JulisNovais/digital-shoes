135b8f171f14d6d45039aac78468536f23b484da
CMCCVR <cmccvr@gmail.com>
2023-01-24 20:14:36 -0300

    adicionar os PRODUTOS

--- src/Products.jsx
+++ src/Products.jsx
@@ -1,9 +1,17 @@
 function Products() {
-    return (
-      <div>
-        PRODUCTS
-      </div>
-    );
-  }
-  
-  export default Products;
+  return (     
+    <div>
+        <h2>Ténis</h2>
+          <section>
+            <div>
+              <img src="https://imgcentauro-a.akamaihd.net/400x400/9789492R/tenis-nike-revolution-6-masculino-img.jpg" alt=""/>
+              <p></p>
+              <p>Price : <span>R$50</span></p>
+              {/* <link to>"/Carrinho</link>   */}
+            </div> 
+          </section>
+    </div>
+  );
+}
+
+export default Products;
